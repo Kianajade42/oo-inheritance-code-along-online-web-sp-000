5cab5fe2d39765b3d558e40b75b5eb32d6d83685
Kiana Long <kianajade42.kl@gmail.com>
2021-02-07 18:26:20 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,10 +3,10 @@ class Vehicle
  def initialize(wheel_size,wheel_number)
    @wheel_size=wheel_size
    @wheel_number=wheel_number
- end 
-  def go 
+ end
+  def go
     "vrrrrrrrooom!"
-  end 
+  end
   def fill_up_tank
     "filling up!"
  end 
